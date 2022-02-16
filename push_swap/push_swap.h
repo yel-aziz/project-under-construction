b776416509c3dab342086aae583bfae8e09f626d
Yassir El azizi <88348802+yel-aziz@users.noreply.github.com>
2022-02-16 23:03:20 +0100

    Add files via upload

---
+++ push_swap/push_swap.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-aziz <yel-aziz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/14 20:35:42 by yel-aziz          #+#    #+#             */
+/*   Updated: 2022/02/16 22:22:28 by yel-aziz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include<stdlib.h>
+
+void	ft_putchar(char c);
+int		ft_atoi(const char *str);
+
+
+
+#endif
