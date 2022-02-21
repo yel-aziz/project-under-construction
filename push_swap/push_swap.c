466f734bd75009a22e133a570a6224bf2afec4bf
Yassir El azizi <88348802+yel-aziz@users.noreply.github.com>
2022-02-21 22:19:37 +0100

    Add files via upload

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,35 +6,44 @@
 /*   By: yel-aziz <yel-aziz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 19:58:53 by yel-aziz          #+#    #+#             */
-/*   Updated: 2022/02/16 22:23:25 by yel-aziz         ###   ########.fr       */
+/*   Updated: 2022/02/21 22:18:16 by yel-aziz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	index_stack(int len_argument)
+{
+	int	index;
+
+	index = len_argument - 1;
+
+	return (index);
+}
+
+int	*sa_swap(int *table, int index_stack)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	b = 0;
+	if(index_stack <= 0)
+		exit (0);
+		a = table[index_stack];
+		b = table[index_stack - 1];
+		table[index_stack - 1] = a;
+		table[index_stack] = b;
+		return (table);
+}
+
+void push(t_stack *a, int k)
+{
+	a->top++;
+	a->items[a->top] = k;
+}
+
 int main(int ac, char **av )
 {
-
-	int *tmp_1;
-	ac = 0;
-	int i = 1;
-	int k = 0;
-
-	while (av[i])
-	{
-		i++;
-	}
-	tmp_1 = malloc(i *sizeof(int));
-	i = 1;
-	while (av[i])
-	{
-		tmp_1[k++] = ft_atoi(av[i]);
-		i++;
-	}
-	k =0;
-	while (k < i - 1)
-	printf("%d\n",tmp_1[k++]);
-
-	
 	
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: yel-aziz <yel-aziz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 20:35:42 by yel-aziz          #+#    #+#             */
-/*   Updated: 2022/02/16 22:22:28 by yel-aziz         ###   ########.fr       */
+/*   Updated: 2022/02/21 22:16:33 by yel-aziz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,16 @@
 #include <stdio.h>
 #include<stdlib.h>
 
+typedef	struct s_stack
+{
+	int top;
+	int capacity;
+	int	*items;
+}t_stack;
+
 void	ft_putchar(char c);
 int		ft_atoi(const char *str);
+int		*push_a(int *b, int index_stack_b);
 
 
 
